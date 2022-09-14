8f04fd04147f26e0fc033bbcc3ccc6145e7b7d1a
Theo Gill <theogill1369@gmail.com>
2022-09-14 15:10:28 -0400

    instatiated DispalyEntries component on app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { useState } from 'react';
+import DisplayPost from './Components/DisplayPost/DisplayPost';
 
 
 function App() {
 
-  const [entries, setEntries] = useState([{name: 'David Lagrange', post: 'I love playing guitar. Does anyone want to play with me?'}])
+  const [entries, setEntries] = useState([{name: "David Lagrange", post: "I love playing guitar. Does anyone want to play with me?"}])
 
 
   return (
     <div className="App">
-      <table>
-        <thead>
-        <tr>
-            <th>Name</th>
-            <th>Post</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>"David Lagrange"</td>
-          <td>'I love playing guitar. Does anyone want to play with me?'</td>
-        </tr>
-      </tbody>
-    </table>  
+       <DisplayPost parentEntries={entries} /> 
   </div>
-  );}
+  );
+}
 
 export default App;
